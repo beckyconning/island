d91b814dc7947484e8f0a8ad4fcefad278cd547e
Becky Conning <becky.conning@gmail.com>
2012-10-23 05:20:49 +0100

    refactored world.js

--- features/support/world.js
+++ features/support/world.js
@@ -2,8 +2,7 @@ var EnvironmentWorld = function EnvironmentWorld(callback) {
   var worldType = process.env.WORLD_TYPE || 'direct_access';
 }
 
-EnvironmentWorld.prototype.World = function() {
-  require('./' + worldType + '_world').World;
-};
+EnvironmentWorld.prototype.World =require('./' + worldType + '_world').World;
+
 
 exports.World = EnvironmentWorld.World;
