c83ef53a2844d8d4da09f3625794bb516a66b09e
Becky Conning <becky.conning@gmail.com>
2012-10-23 05:26:30 +0100

    fixed cucumber env variable world selector

--- features/support/world.js
+++ features/support/world.js
@@ -1,9 +1,3 @@
-var EnvironmentWorld = function EnvironmentWorld(callback) {
-}
-
 var worldType = process.env.WORLD_TYPE || 'direct_access';
 
-EnvironmentWorld.prototype.World = require('./' + worldType + '_world').World;
-
-
-exports.World = EnvironmentWorld.World;
+exports.World = require('./' + worldType + '_world').World;
